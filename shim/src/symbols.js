6fe0e5f014319335f62d140825a3cd8aac956229
JF Paradis <jparadis@salesforce.com>
2018-05-20 10:45:20 -0700

    Removed shim

--- shim/src/symbols.js
+++
@@ -1,5 +0,0 @@
-export const RealmRecord = Symbol('Realm Slot');
-export const Intrinsics = Symbol('Intrinsics Slot');
-export const GlobalObject = Symbol('GlobalObject Slot');
-export const DirectEvalEvaluator = Symbol('DirectEvalEvaluator Slot');
-export const ShimSandbox = Symbol('Shim Sandbox');
